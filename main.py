3aa00cd7d4574dbac80320876958c7d4d6fa5a1b
CarcinoArison <45807691+CarcinoArison@users.noreply.github.com>
2018-12-23 12:11:34 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+playerMove = input("Scissors, Paper, or Rock? ")
+
+def pickRandom():
+  import random 
+  for x in range(1)
+    random.randint(1,4)
+    
+comp = pickRandom()
+
+if comp == 1:
+  compMove = Rock
+elif comp == 2:
+  compMove = Scissors
+else
+  compMove = Paper
+  
+  print(playerMove)
+  print(compMove)
+  
